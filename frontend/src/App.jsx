7c482bd01e797db20d373fb8ecb0036ec92fa651
cmcejas <carlosmcejas@gmail.com>
2026-03-10 15:47:08 +0000

    pretty ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,32 @@ function App() {
 
   return (
     <>
-      <div class="pointer-events-auto mx-auto w-full max-w-[1600px] overflow-x-auto ">
-        <header class="flex min-w-max items-center gap-4 rounded-2xl border  px-6 py-3  backdrop-blur-xl m-2">
-          <div class="flex items-center gap-3">
-            <button
-              type="button"
-              class="flex items-center gap-3 transition-colors hover:opacity-80"
-            >
-              <h1 class="text-sm font-bold uppercase tracking-[0.2em] whitespace-nowrap">
-                Flight History
-              </h1>
-            </button>
-          </div>
-          <nav class="flex flex-1 items-center justify-center gap-1"></nav>
-        </header>
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-[#13162c] flex justify-center items-start p-10">
+        <div className="pointer-events-auto mx-auto w-full max-w-[1600px]">
+          <header className="flex min-w-max items-center gap-4 m-2 px-6 py-3
+            rounded-[16px]
+            bg-white/10
+            shadow-[0_4px_30px_rgba(0,0,0,0.5)]
+            backdrop-blur-[10px]
+            border border-white/20">
+            
+            <div className="flex items-center gap-3">
+              <button
+                type="button"
+                className="flex items-center gap-3 px-4 py-2 rounded-full
+                  text-white font-semibold
+                  transition-all hover:opacity-90"
+              >
+                <h1 className="text-sm font-bold uppercase tracking-[0.2em] whitespace-nowrap">
+                  Flight History
+                </h1>
+              </button>
+            </div>
+
+            <nav className="flex flex-1 items-center justify-center gap-1"></nav>
+
+          </header>
+        </div>
       </div>
     </>
   );
