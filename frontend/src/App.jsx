41a76412c710935107fa912783e09eec1560af76
Michael <michaelcrowley931@gmail.com>
2026-03-10 15:12:59 +0000

    Added navbar and setup tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex justify-center gap-8 my-6">
-        <a
-          href="https://vite.dev"
-          target="_blank"
-          className="hover:scale-105 transition-transform"
-        >
-          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          className="hover:scale-105 transition-transform"
-        >
-          <img src={reactLogo} className="w-24 h-24" alt="React logo" />
-        </a>
+      <div class="pointer-events-auto mx-auto w-full max-w-[1600px] overflow-x-auto ">
+        <header class="flex min-w-max items-center gap-4 rounded-2xl border  px-6 py-3  backdrop-blur-xl m-2">
+          <div class="flex items-center gap-3">
+            <button
+              type="button"
+              class="flex items-center gap-3 transition-colors hover:opacity-80"
+            >
+              <h1 class="text-sm font-bold uppercase tracking-[0.2em] whitespace-nowrap">
+                Flight History
+              </h1>
+            </button>
+          </div>
+          <nav class="flex flex-1 items-center justify-center gap-1"></nav>
+        </header>
       </div>
-
-      <h1 className="text-4xl font-bold text-center mb-6">Vite + React</h1>
-
-      <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 max-w-sm mx-auto text-center mb-6">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition-colors"
-        >
-          count is {count}
-        </button>
-        <p className="mt-4 text-gray-700 dark:text-gray-300">
-          Edit{" "}
-          <code className="bg-gray-200 dark:bg-gray-700 px-1 rounded">
-            src/App.jsx
-          </code>{" "}
-          and save to test HMR
-        </p>
-      </div>
-
-      <p className="text-center text-gray-600 dark:text-gray-400">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
