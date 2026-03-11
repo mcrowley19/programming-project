d238c135cd4d5115b5f18fd5c1574b76e19686dc
Alper Ergüne <alperergune8@outlook.com>
2026-03-11 23:32:20 +0000

    Flights as objects with attributes added from CSV

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,41 @@
 import csv
-with open('backend/data/flightData.csv', newline='') as csvfile:
+class Flight:
+    def __init__(self, FL_DATE, MKT_CARRIER, MKT_CARRIER_FL_NUM, ORIGIN, ORIGIN_CITY_NAME, ORIGIN_STATE_ABR, ORIGIN_WAC, DEST, DEST_CITY_NAME, DEST_STATE_ABR, DEST_WAC, CRS_DEP_TIME, DEP_TIME, CRS_ARR_TIME, ARR_TIME, CANCELLED, DIVERTED, DISTANCE):
+        self.FL_DATE = FL_DATE
+        self.MKT_CARRIER = MKT_CARRIER
+        self.MKT_CARRIER_FL_NUM = MKT_CARRIER_FL_NUM
+        self.ORIGIN = ORIGIN
+        self.ORIGIN_CITY_NAME = ORIGIN_CITY_NAME
+        self.ORIGIN_STATE_ABR = ORIGIN_STATE_ABR
+        self.ORIGIN_WAC = ORIGIN_WAC
+        self.DEST = DEST
+        self.DEST_CITY_NAME = DEST_CITY_NAME
+        self.DEST_STATE_ABR = DEST_STATE_ABR
+        self.DEST_WAC = DEST_WAC
+        self.CRS_DEP_TIME = CRS_DEP_TIME
+        self.DEP_TIME = DEP_TIME
+        self.CRS_ARR_TIME = CRS_ARR_TIME
+        self.ARR_TIME = ARR_TIME
+        self.CANCELLED = CANCELLED
+        self.DIVERTED = DIVERTED
+        self.DISTANCE = DISTANCE
+    
+    def flightDetails(self):
+        return f'Flight Date: {self.FL_DATE}\nMarket Carrier: {self.MKT_CARRIER}\nMarket Carrier Flight Number: {self.MKT_CARRIER_FL_NUM}\nOrigin: {self.ORIGIN}\nOrigin City Name: {self.ORIGIN_CITY_NAME}\nOrigin State Abbreviation: {self.ORIGIN_STATE_ABR}\nOrigin WAC: {self.ORIGIN_WAC}\nDestination: {self.DEST}\nDestination City Name: {self.DEST_CITY_NAME}\nDestination State Abbreviation: {self.DEST_STATE_ABR}\nDestination WAC: {self.DEST_WAC}\nCRS Departure Time: {self.CRS_DEP_TIME}\nDeparture Time: {self.DEP_TIME}\nCRS Arrival Time: {self.CRS_ARR_TIME}\nArrival Time: {self.ARR_TIME}\nCancelled: {self.CANCELLED}\nDiverted: {self.DIVERTED}\nDistance: {self.DISTANCE}'
+
+
+flightList = []
+with open('backend/data/flightData.csv') as csvfile:
     reader = csv.reader(csvfile)
+    flightCounter = 0
     for row in reader:
-        print(row)
+        flightCounter += 1
+        flightDataString = str(row).replace(',\t', ' ') # replacing commas inside origin/destination city names with space
+        print(flightDataString)
+        try:
+            FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME,ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME,DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE = flightDataString.split(sep=',', maxsplit=17)
+            tempFlight = Flight(FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME,ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME,DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE)
+            flightList.append(tempFlight)
+            print('Flight counter:', flightCounter, '\n', tempFlight.flightDetails(), end='\n\n')
+        except Exception as e:
+            print(f"Error with {flightCounter}: {e}")
