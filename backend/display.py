cc8b9731645facf1efe69cfeb2f1e8985348e811
Cillian <h84680199@gmail.com>
2026-03-12 12:08:06 +0000

    changes as per branch name

--- backend/display.py
+++ backend/display.py
@@ -1,8 +1,11 @@
 import plotly.express as px
 import pandas as pd
+pd.options.plotting.backend = "plotly"
 
 df = pd.read_csv("backend/data/flightDataCleaned.csv")
 
+lateFlights = df.groupby("MKT_CARRIER")[["LATE", "ON_TIME"]].sum().reset_index()
+carriers = [df["MKT_CARRIER"].unique()]
 
 counts_list = [df['FL_DATE'].value_counts().get('1/1/2022', 0),
                df['FL_DATE'].value_counts().get('1/2/2022', 0),
@@ -14,6 +17,17 @@ counts_list = [df['FL_DATE'].value_counts().get('1/1/2022', 0),
 data_canada = px.data.gapminder().query("country == 'Canada'")
 fig = px.bar(data_canada, x=df["FL_DATE"].unique().tolist(), y=counts_list,
              labels=dict(x="Date", y="Flights count"))
+
+
+
+
+
+fig2 =lateFlights.plot(kind="bar", x="MKT_CARRIER", y=["ON_TIME", "LATE"],
+                       labels = dict(x="Amount of flights", y = "Carrier"))
+fig2.show()
+
+print(lateFlights)
+
 fig.show()
 
 
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import csv
+import pandas as pd
+
 class Flight:
     def __init__(self, FL_DATE, MKT_CARRIER, MKT_CARRIER_FL_NUM, ORIGIN, ORIGIN_CITY_NAME, ORIGIN_STATE_ABR, ORIGIN_WAC, DEST, DEST_CITY_NAME, DEST_STATE_ABR, DEST_WAC, CRS_DEP_TIME, DEP_TIME, CRS_ARR_TIME, ARR_TIME, CANCELLED, DIVERTED, DISTANCE):
         self.FL_DATE = FL_DATE
@@ -29,6 +31,24 @@ class Flight:
     def flightDetails(self):
         return f'Flight Date: {self.FL_DATE}\nMarket Carrier: {self.MKT_CARRIER}\nMarket Carrier Flight Number: {self.MKT_CARRIER_FL_NUM}\nOrigin: {self.ORIGIN}\nOrigin City Name: {self.ORIGIN_CITY_NAME}\nOrigin State Abbreviation: {self.ORIGIN_STATE_ABR}\nOrigin WAC: {self.ORIGIN_WAC}\nDestination: {self.DEST}\nDestination City Name: {self.DEST_CITY_NAME}\nDestination State Abbreviation: {self.DEST_STATE_ABR}\nDestination WAC: {self.DEST_WAC}\nCRS Departure Time: {self.CRS_DEP_TIME}\nDeparture Time: {self.DEP_TIME}\nCRS Arrival Time: {self.CRS_ARR_TIME}\nArrival Time: {self.ARR_TIME}\nCancelled: {self.CANCELLED}\nDiverted: {self.DIVERTED}\nDistance: {self.DISTANCE}'
 
+
+flightList = []
+with open('backend/data/flightData.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    flightCounter = 0
+    for row in reader:
+        flightCounter += 1
+        flightDataString = str(row).strip('[]')
+        try:
+            FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME, OriginCityExt, ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME, DestCityExt, DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE = flightDataString.split(',')
+            # OriginCityExt and DestCityExt are for the seperated state parts of the names because of the commas inbetween them and the city names.
+            tempFlight = Flight(FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME + ',' + OriginCityExt,ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME + ',' + DestCityExt,DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE)
+            flightList.append(tempFlight)
+        except Exception as e:
+            print(f"Error with {flightCounter}: {e}")
+
+#for flight in flightList:
+#    print(f'{flight.flightDetails()}\n')
 def load_flights():
     flightList = []
     with open('backend/data/flightData.csv') as csvfile:
@@ -64,11 +84,27 @@ def cancel_filter(flightList, state):
             cancelledFlights.append(flight)
     return cancelledFlights
 
+def late_flights(df):
+    df['LATE'] = df['CRS_ARR_TIME'] < df['ARR_TIME']
+    df["ON_TIME"] = (df['CRS_ARR_TIME'] >= df['ARR_TIME']) & (df['CANCELLED'] == 0)
+    return df
+
+def flight_snipper(df):
+    df["FL_DATE"] = df["FL_DATE"].str.slice(0, -12)
+    return df
+
+
+def flight_cleaner():
+    df = pd.read_csv("backend/data/flightData.csv")
+    df = late_flights(df)
+    df = flight_snipper(df)
+    df.to_csv("backend/data/flightDataCleaned.csv", index=False)
 
 
 
 def main():
     flightList = load_flights()
+    flight_cleaner()
 
     for flight in airport_origin_filter(flightList, 'ATL'):
         print(f'{flight.flightDetails()}\n')
