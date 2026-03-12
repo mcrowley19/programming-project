82bdaf590fce9e83cf609d0fe3bd2d4361110a2f
Alper Ergüne <alperergune8@outlook.com>
2026-03-12 10:51:09 +0000

    Backend Refactoring

--- backend/main.py
+++ backend/main.py
@@ -29,27 +29,27 @@ class Flight:
     def flightDetails(self):
         return f'Flight Date: {self.FL_DATE}\nMarket Carrier: {self.MKT_CARRIER}\nMarket Carrier Flight Number: {self.MKT_CARRIER_FL_NUM}\nOrigin: {self.ORIGIN}\nOrigin City Name: {self.ORIGIN_CITY_NAME}\nOrigin State Abbreviation: {self.ORIGIN_STATE_ABR}\nOrigin WAC: {self.ORIGIN_WAC}\nDestination: {self.DEST}\nDestination City Name: {self.DEST_CITY_NAME}\nDestination State Abbreviation: {self.DEST_STATE_ABR}\nDestination WAC: {self.DEST_WAC}\nCRS Departure Time: {self.CRS_DEP_TIME}\nDeparture Time: {self.DEP_TIME}\nCRS Arrival Time: {self.CRS_ARR_TIME}\nArrival Time: {self.ARR_TIME}\nCancelled: {self.CANCELLED}\nDiverted: {self.DIVERTED}\nDistance: {self.DISTANCE}'
 
-
-flightList = []
-with open('backend/data/flightData.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    flightCounter = 0
-    for row in reader:
-        flightCounter += 1
-        flightDataString = str(row).strip('[]')
-        try:
-            FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME, OriginCityExt, ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME, DestCityExt, DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE = flightDataString.split(',')
-            # OriginCityExt and DestCityExt are for the seperated state parts of the names because of the commas inbetween them and the city names.
-            tempFlight = Flight(FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME + ',' + OriginCityExt,ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME + ',' + DestCityExt,DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE)
-            flightList.append(tempFlight)
-        except Exception as e:
-            print(f"Error with {flightCounter}: {e}")
-
-for flight in flightList:
-    print(f'{flight.flightDetails()}\n')
-
+def load_flights():
+    flightList = []
+    with open('backend/data/flightData.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        flightCounter = 0
+        for row in reader:
+            flightCounter += 1
+            flightDataString = str(row).strip('[]')
+            try:
+                FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME, OriginCityExt, ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME, DestCityExt, DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE = flightDataString.split(',')
+                # OriginCityExt and DestCityExt are for the seperated state parts of the names because of the commas inbetween them and the city names.
+                tempFlight = Flight(FL_DATE,MKT_CARRIER,MKT_CARRIER_FL_NUM,ORIGIN,ORIGIN_CITY_NAME + ',' + OriginCityExt,ORIGIN_STATE_ABR,ORIGIN_WAC,DEST,DEST_CITY_NAME + ',' + DestCityExt,DEST_STATE_ABR,DEST_WAC,CRS_DEP_TIME,DEP_TIME,CRS_ARR_TIME,ARR_TIME,CANCELLED,DIVERTED,DISTANCE)
+                flightList.append(tempFlight)
+            except Exception as e:
+                print(f"Error with {flightCounter}: {e}")
     
-def airport_origin_filter(airport_code):
+    return flightList
+
+
+def airport_origin_filter(flightList, airport_code):
     filteredFlights = []
     for flight in flightList:
         if flight.ORIGIN == f" '{airport_code}'":
@@ -57,15 +57,24 @@ def airport_origin_filter(airport_code):
     return filteredFlights
 
 
-def cancel_filter(state):
+def cancel_filter(flightList, state):
     cancelledFlights = []
     for flight in flightList:
-        if flight.CANCELLED == f" '{state}'":
+        if flight.CANCELLED == state:
             cancelledFlights.append(flight)
     return cancelledFlights
 
 
-#for flight in airport_origin_filter('ATL'):
-    print(f'{flight.flightDetails()}\n')
-#for flight in cancel_filter(True):
-    print(f'{flight.flightDetails()}\n')
+
+
+def main():
+    flightList = load_flights()
+
+    for flight in airport_origin_filter(flightList, 'ATL'):
+        print(f'{flight.flightDetails()}\n')
+
+    #for flight in cancel_filter(flightList, True):
+    #    print(f'{flight.flightDetails()}\n')
+
+if __name__ == '__main__':
+    main()
