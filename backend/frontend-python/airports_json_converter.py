956c625a729a1355e12f57b6df911b8049306468
Michael <michaelcrowley931@gmail.com>
2026-03-16 13:16:03 +0000

    Added location tags to map file but they are not loading in.

---
+++ backend/frontend-python/airports_json_converter.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import json
+
+""""
+Format needed for frontend
+    "0": {
+      name: "New York",
+      lat: "40.71",
+      lng: "-74.0059731"
+    },
+    "1": {
+      name: "Chicago, IL",
+      lat: "41.9288",
+      lng: "-87.6315",
+    },
+    "3": {
+      lat: 61.218,
+      lng: -149.858,
+      name: "Anchorage"
+    }
+"""
+df = pd.read_csv('programming-project/backend/data/airportsCleaned.csv')
+
+airports_dict = {}
+
+iata_list = df["IATA"].tolist()
+long_list = df["LATITUDE"].tolist()
+lat_list = df["LONGITUDE"].tolist()
+
+for iata, long, lat in zip(iata_list, long_list, lat_list):
+    current_airport = {
+        "lat":lat,
+        "lng": long,
+        "name": iata
+        }
+    airports_dict[iata] = current_airport
+
+# This outputs a json string which has quotes around the key names. To solve this I did JSON.parse in the js code
+print(json.dumps(airports_dict))
+
+
+
+
+
