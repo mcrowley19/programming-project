e651199be5f9875c03065160a220a056a4cbddb3
Michael <michaelcrowley931@gmail.com>
2026-03-17 21:01:49 +0100

    Removed non-mainland US airports

--- backend/dataCleaningMain.py
+++ backend/dataCleaningMain.py
@@ -1,14 +1,47 @@
 import pandas as pd
+import json
+
+""""
+Format needed for frontend
+    "0": {
+      name: "New York",
+      lat: "40.71",
+      lng: "-74.0059731"
+    },
+    "1": {
+      name: "Chicago, IL",
+      lat: "41.9288",
+      lng: "-87.6315",
+    },
+    "3": {
+      lat: 61.218,
+      lng: -149.858,
+      name: "Anchorage"
+    }
+"""
+df = pd.read_csv('../data/airportsCleaned.csv')
+
+airports_dict = {}
+
+iata_list = df["IATA"].tolist()
+long_list = df["LONGITUDE"].tolist()
+lat_list = df["LATITUDE"].tolist()
+
+non_mainland_airports = ["ANC", "FAI", "JNU", "KTN", "SIT", "HNL", "OGG", "KOA", "ITO", "LIH", "SJU", "BQN", "PSE", 
+                         "GUM", "STT", "STX", "PPG", "SPN", "ROP", "TIQ","ADK", "ADQ", 
+                         "AKN", "BET", "BRW", "CDV", "DLG", "GST", "OME", "OTZ", "PSG", "SCC", "WRG", "YAK"]
+for iata, long, lat, count in zip(iata_list, long_list, lat_list, range(len(long_list))):
+    if iata not in non_mainland_airports:
+      current_airport = {
+          "lat":str(round(lat,2)),
+          "lng": str(round(long, 2)),
+          "name": iata
+          }
+      airports_dict[count] = current_airport
+
+# This outputs a json string which has quotes around the key names. To solve this I did JSON.parse in the js code
+print(json.dumps(airports_dict))
 
 
-#literally just removes the last 12 characters i.e. the "12:00:00AM" from the date line
 
-flightData = pd.read_csv("backend/data/flightData.csv")
-
-flightData["FL_DATE"] = flightData["FL_DATE"].str.slice(0, -12)
-
-
-flightData.to_csv("backend/data/flightDataCleaned.csv", index=False)
-
-        
 
