507feb1836a02530360f545e0da1a87ed9001ec4
aaronfoley <aaronfoley97@gmail.com>
2026-03-22 17:17:41 +0000

    temporaty flight path frontend

--- backend/display.py
+++ backend/display.py
@@ -1,8 +1,9 @@
 import plotly.express as px
 import pandas as pd
-
+pd.options.plotting.backend = "plotly"
 df = pd.read_csv("backend/data/flightDataCleaned.csv")
-
+lateFlights = df.groupby("MKT_CARRIER")[["LATE", "ON_TIME"]].sum().reset_index()
+carriers = [df["MKT_CARRIER"].unique()]
 
 counts_list = [df['FL_DATE'].value_counts().get('1/1/2022', 0),
                df['FL_DATE'].value_counts().get('1/2/2022', 0),
@@ -11,9 +12,17 @@ counts_list = [df['FL_DATE'].value_counts().get('1/1/2022', 0),
                df['FL_DATE'].value_counts().get('1/5/2022', 0),
                df['FL_DATE'].value_counts().get('1/6/2022', 0),
                ]
+fig2 =lateFlights.plot(kind="bar", x="MKT_CARRIER", y=["ON_TIME", "LATE"],
+                       labels = dict(x="Amount of flights", y = "Carrier"))
+fig2.show()
+
+print(lateFlights)
 data_canada = px.data.gapminder().query("country == 'Canada'")
 fig = px.bar(data_canada, x=df["FL_DATE"].unique().tolist(), y=counts_list,
              labels=dict(x="Date", y="Flights count"))
+fig.show()
+top10=df['ORIGIN'].value_counts().nlargest(10).reset_index()
+fig = px.bar(top10, x='ORIGIN', y='count', labels={'ORIGIN': 'Origin Airport', 'count': 'Number of Flights'})
 fig.show()
 
 
